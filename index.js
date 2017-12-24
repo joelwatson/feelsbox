c9d2ff5abbd0bd761976e59451f69cc752e6aae9
root <existdissolve@gmail.com>
2017-12-24 17:41:13 -0600

    Updates to inverting row values, weather handling

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import Matrix from 'rpi-ws281x-native';
 import io from 'socket.io-client';
 import request from 'request';
+import {Gpio} from 'onoff';
+
 import {apiKey, latitude, longitude} from './config';
+import {love} from './pixels/default-emoji';
+import numbers from './pixels/numbers';
 
 const ledCount = 64;
-const socket = io('http://localhost:3000');
+const socket = io('https://feelsbox.herokuapp.com', {forceNew: true});
 const weatherEndPoint = 'https://api.darksky.net/forecast';
+const toggle = new Gpio(4, 'in', 'both');
+let viewState = 0;
 let currentWeather = {};
 let weatherReqFn;
+
+toggle.watch((err, value) => {
+    if (!err && value) {
+        setViewState();
+    }
+});
 
 const getWeather = () => { 
     const opts = {
@@ -39,27 +51,128 @@ const getWeather = () => {
                 high = max;
             }
         });
-        
-        currentWeather = {
+
+        renderWeather({
             icon,
             current,
-            feelslike,
+            feelslike: parseInt(feelslike),
             low,
             high
-        };
+        });
     });
 };
 
-socket.on('emote', feeling => {
+const renderWeather = weather => {
+    const {feelslike, icon} = weather;
+    const temperature = Math.min(Math.abs(feelslike), 99).toString().split('');
+    const pixelData = new Uint32Array(ledCount);
+    let borderColor;
+    
+    if (temperature.length === 1) {
+        // normalize entry to we always have 2 places
+        temperature.unshift('_');
+    }
+
+    // initialize Matrix if not already initialized
+    if (Matrix.state !== 'weather') {
+        Matrix.init(ledCount, {brightness: 20});
+        Matrix.state = 'weather';
+    }
+
+    temperature.forEach((entry, index) => {
+        if (entry === '_') {
+            return;
+        }
+
+        const digit = numbers[entry];
+        const offset = index === 0 ? 0 : 3;
+        let start = 9 + offset;
+        let current = start;
+
+        // loop over rows      
+        for (let i=0; i<5; i++) {
+            start = start + 8;
+            current = start - 1;
+
+            digit[i].forEach((row, idx) => {
+                current++;
+                if (row) {
+                    pixelData[invertValue(current)] = '0xffffff';
+                }                
+            });
+        }
+    });
+
+    Matrix.render(pixelData);
+};
+
+const showFeeling = feeling => {
     Matrix.init(ledCount, {brightness: 50});
+    Matrix.state = 'feeling';
     const pixelData = new Uint32Array(ledCount);
 
     feeling.forEach(function (item) {
         const {i, c} = item;
-        pixelData[i-1] = `0x${c}`;
+        pixelData[invertValue(i-1)] = `0x${c}`;
     });
-
+    console.log('a feeling happened');
     Matrix.render(pixelData);
+};
+
+const clearMatrix = () => {
+    const {state = null} = Matrix;
+
+    if (state) {
+        Matrix.reset();
+        delete Matrix.state;
+    }    
+};
+
+const setViewState = () => {
+    // we always need to clear the matrix between states
+    clearMatrix();
+    
+    switch(viewState) {
+        case 1: // emoji
+            viewState = 2;
+            getWeather();
+            //weatherReqFn = setInterval(getWeather, 600000);
+	        break;
+	    case 2: // weather
+            viewState = 0;
+	        break;
+	    default: // screen off
+            viewState = 1;
+            showFeeling(love);
+	        break;
+    }
+};
+
+const invertValue = value => {
+    const row = parseInt(value / 8);
+    const needsInversion = !(row % 2);
+    let fixedValue = value;
+
+    if (needsInversion) {
+        const rowStart = row * 8;
+        const rowEnd = rowStart + 7;
+        const distanceFromStart = value - rowStart;
+        
+        fixedValue = rowEnd - distanceFromStart;
+    }
+
+    return fixedValue;
+};
+
+const exitHandler = () => {
+    socket.close();
+    clearMatrix();
+    clearInterval(weatherReqFn);
+}
+
+socket.on('emote', feeling => {
+    viewState = 1;
+    showFeeling(feeling);
 });
 
 socket.on('stop', () => {
@@ -68,14 +181,8 @@ socket.on('stop', () => {
 });
 
 socket.on('weather', () => {
-    getWeather();
-    //weatherReqFn = setInterval(getWeather, 600000);
+    weatherReqFn = setInterval(getWeather, 600000);
 });
-
-function exitHandler() {
-    Matrix.reset();
-    clearInterval(weatherReqFn);
-}
 
 //do something when app is closing
 process.on('exit', exitHandler);
